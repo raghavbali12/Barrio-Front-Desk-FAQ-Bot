6e82c55312d79a936ddf899fb0353a87aa806f73
raghavbali12 <34580624+raghavbali12@users.noreply.github.com>
2024-09-02 20:54:37 -0500

    chore: Update webchat bot URL with secret token

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 
 function App() {
+  const secret = import.meta.env.VITE_APP_SECRET;
+
   return (
     <div>
       <h1>Barrio Front Desk FAQ Bot</h1>
       <iframe
-        src="https://webchat.botframework.com/embed/RB-Language-Service-bot?s=YOUR_SECRET"
+        src={
+          "https://webchat.botframework.com/embed/RB-Language-Service-bot?s=" +
+          secret
+        }
         style={{ minWidth: "400px", width: "100%", minHeight: "500px" }}
       ></iframe>
     </div>
