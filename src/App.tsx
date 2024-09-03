bb4bd515fe5777fe115f2ac5eac28a8af2603002
raghavbali12 <34580624+raghavbali12@users.noreply.github.com>
2024-09-02 20:39:10 -0500

    Initial code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Barrio Front Desk FAQ Bot</h1>
+      <iframe
+        src="https://webchat.botframework.com/embed/RB-Language-Service-bot?s=YOUR_SECRET"
+        style={{ minWidth: "400px", width: "100%", minHeight: "500px" }}
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
